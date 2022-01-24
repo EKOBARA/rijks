44c8c5890905beae2ce3866c46a1816d1531bdd9
ekobara <emmanuelkobara@gmail.com>
2022-01-24 15:53:53 -0500

    Initial commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,30 @@
-import logo from './logo.svg';
+import React, { useState } from 'react';
+import data from './data.json';
+import CarouselContainer from './components/CarouselContainer';
+import Navigation from './components/Navigation';
 import './App.css';
+import { Navigate, Outlet, Route, Routes } from 'react-router-dom';
 
 function App() {
+  const [searchOptions, setSearchOptions] = useState({
+		key: process.env.REACT_APP_RIJKS_KEY,
+		url: 'https://www.rijksmuseum.nl/api/en/',
+		numberOfResults: 15,
+	});
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <Navigation />
+      <main>
+        <Routes>
+          <Route 
+            exact
+            path='/home' 
+            element={<CarouselContainer data={data}/>} 
+          />
+          <Route path='*' element={<Navigate to='/home' />} /> 
+        </Routes>
+      </main>
+    </>
   );
 }
 
